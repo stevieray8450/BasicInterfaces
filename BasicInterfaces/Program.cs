c3715f19ca09e0ff7dbe3a36e773db52c3c321cb
stevieray8450 <stevieray8450@gmail.com>
2017-02-26 17:06:17 -0600

    first commit 02.26

--- BasicInterfaces/Program.cs
+++ BasicInterfaces/Program.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace BasicInterfaces
 {
+    interface IStorable
+    {
+        void Save();
+        void Load();
+    }
+
+    interface IPrintable
+    {
+        void Print();
+    }
+
+    class Document : IStorable, IPrintable
+    {
+        public void Save()
+        {
+            Console.WriteLine("Document saved");
+        }
+        public void Load()
+        {
+            Console.WriteLine("Document loaded");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Document printing");
+        }
+    }
+
+    class Image : Document
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Image printed");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Document d = new Document();
+            d.Save();
+            d.Load();
+            d.Print();
+
+            Console.WriteLine("*********************");
+
+            Image i = new Image();
+            i.Save();
+            i.Load();
+            //i.Print();
         }
     }
 }
